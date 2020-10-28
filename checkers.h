64e8bffab7a9529363e146ce4cfb884dce58c4ed
Matt <matthewliuny@gmail.com>
2020-10-28 10:01:40 -0400

    initilazed board

---
+++ checkers.h
@@ -0,0 +1,17 @@
+/* 
+ * checkers.h
+ */
+
+#ifndef checkers
+#define checkers
+
+#define boardSize 8
+
+char board[boardSize][boardSize];
+
+void initBoard(void);
+
+void drawBoard(void);
+
+
+#endif
--- main.c
+++ main.c
@@ -1,2 +1,16 @@
-blah blah blah:wq
+/*
+ * main.c
+ */
 
+#include <stdio.h>
+#include "checkers.h"
+
+void initBoard(void);
+
+char board[boardSize][boardSize];
+
+int main() 
+{
+    initBoard();
+
+}
