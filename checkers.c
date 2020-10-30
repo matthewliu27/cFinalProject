a03cc8b07819d20d88d5fdffbdbe93b35a4882de
Matt <matthewliuny@gmail.com>
2020-10-30 19:04:37 -0400

    fixed issue

--- checkers.c
+++ checkers.c
@@ -23,16 +23,14 @@ void initBoard(void)
 void drawBoard(void)
 {
     int row;
-    row=0;
     printf("\n");
-    while (row < 8)
+   for ( row = 0; row < boardSize ; row++)  
     {
         printf("   *   *   *   *   *   *   *   *\n");
         printf(" %c * %c * %c * %c * %c * %c * %c * %c *\n", board[row][0], board[row][1], board[row][2], board[row][3], board[row][4], board[row][5],
                                   board[row][6], board[row][7]);
         printf("   *   *   *   *   *   *   *   *\n");
         row++;
-        return;
     }
 }
 
