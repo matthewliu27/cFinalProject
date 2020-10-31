0b6cd51ba5f6794ae14b6004a45fdd36ca5f09a5
Matt <matthewliuny@gmail.com>
2020-10-30 20:24:36 -0400

    fixed init piece positions

--- checkers.c
+++ checkers.c
@@ -6,40 +6,41 @@
 #include "checkers.h"
 
 void initBoard(void)
- {
-                     int col;
+{ 
+int col;
 
-                     {
-                         for(col =0; col<boardSize; col++)
-                         {
-                             if(col% 2 == 0)
-                             {
-                                 board[5][col]= 'x';
-                                 board[7][col] = 'x';
-                                 board[0][col]= 'o';
-                                 board[2][col] = 'o';
-                             }
-                             if(col % 2 == 1)
-                             {
-                                 board[1][col] = 'o';
-                                 board[6][col] = 'x';
-            }
-      }
+{
+for(col =0; col<boardSize; col++)
+   {
+    if(col % 2 == 0)
+    {
+        board[5][col]= 'x';
+        board[7][col] = 'x';
+        board[1][col]= 'o';
+     }
+
+    if(col % 2 == 1) 
+    {
+        board[0][col] = 'o';
+        board[6][col] = 'x';
+        board[2][col] = 'o';
+    }
+   }
    return;
+ 
  }
- }
-
+}
 void drawBoard(void)
 {
     int row;
     printf("\n");
    for ( row = 0; row < boardSize ; row++)  
     {
-        printf("   *   *   *   *   *   *   *   *\n");
-        printf(" %c * %c * %c * %c * %c * %c * %c * %c *\n", board[row][0], board[row][1], board[row][2], board[row][3], board[row][4], board[row][5],
+        printf("   *   *   *   *   *   *   *   \n");
+        printf(" %c * %c * %c * %c * %c * %c * %c * %c \n", board[row][0], board[row][1], board[row][2], board[row][3], board[row][4], board[row][5],
                                   board[row][6], board[row][7]);
-        printf("   *   *   *   *   *   *   *   *\n");
-        printf("***********************************\n");
+        printf("   *   *   *   *   *   *   *   \n");
+        printf("********************************\n");
     }
 }
 
